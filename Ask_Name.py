eabec841ae5fcc716e2f6389af9638ec13682a4b
Vijay Kumar <viju1098@gmail.com>
2019-12-07 13:29:18 +0530

    First Python Program_Version 1

--- Ask_Name.py
+++ Ask_Name.py
@@ -1,6 +1,29 @@
-print("Hello User, what is your name")
-x=input()
-print("My name is :", x)
+print("Machine: Hello User, what is your name")
+print()
+#x= str(input("User: ")))
+#print()
+print("User: My name is :", str(input()))
+#print()
 print("Machine: 'Thank you'")
+print()
 print("Machine: Please confirm your Employee ID")
-y= str(input())
+print()
+print("User: "), str(input())
+response1= 1
+#response2=2
+a=["WIGA", "KMF", "Support Automation", "AI Innovation"]
+print("Machine: Press 1 if you are a PSET aligned Project Manager?")
+print()
+z= int(input("User: "))
+print()
+if (z == response1):
+    print("Machine: Thank you! here's is your project list")
+    print()
+    print(a)
+else :
+    print("Machine: please reach out to your manager for your project list")
+    print()
+    print("Thank you")
+
+
+
